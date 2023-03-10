f5a1e1fc07f68de225f7f77b819a8ae934965ce3
Abigail Sosa <61513075+sadiee08@users.noreply.github.com>
2023-03-10 02:36:36 -0600

    cards: home

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -24,10 +24,10 @@ const Home = () => {
 						<img style={{maxHeight: "70vh", objectFit: "cover"}} src="https://picsum.photos/2000" class="d-block w-100" />
 					</div>
 					<div class="carousel-item">
-						<img style={{maxHeight: "70vh", objectFit: "cover"}} src="https://picsum.photos/2000" class="d-block w-100" />
+						<img style={{maxHeight: "70vh", objectFit: "cover"}} src="https://picsum.photos/2005" class="d-block w-100" />
 					</div>
 					<div class="carousel-item">
-						<img style={{maxHeight: "70vh", objectFit: "cover"}} src="https://picsum.photos/2000" class="d-block w-100" />
+						<img style={{maxHeight: "70vh", objectFit: "cover"}} src="https://picsum.photos/2006" class="d-block w-100" />
 					</div>
 				</div>
 				<button class="carousel-control-prev" type="button" data-bs-target="#logo-carousel" data-bs-slide="prev">
@@ -39,6 +39,36 @@ const Home = () => {
 					<span class="visually-hidden">Next</span>
 				</button>
 			</div>
+			<div class="row row-cols-1 row-cols-md-3 g-4" style={{marginTop: "10px"}}>
+				<div class="col">
+					<div class="card h-100">
+						<img src="https://picsum.photos/2001" class="card-img-top" alt="..."></img>
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col">
+					<div class="card h-100">
+						<img src="https://picsum.photos/2002" class="card-img-top" alt="..."></img>
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col">
+					<div class="card h-100">
+						<img src="https://picsum.photos/2003" class="card-img-top" alt="..."></img>
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
 		</div>
 	);
 };
