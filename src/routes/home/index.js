107c3c56439e1fd67a9cde6fd9b0766460b821b1
Mauricio Alvarado <mauricio.alvaradoh98@gmail.com>
2023-03-17 22:14:25 -0600

    chore: added sample components

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,46 +1,38 @@
-import { h } from "preact";
-import { useEffect } from 'preact/hooks';
-import style from "./style";
+import { useEffect } from "preact/hooks";
+import ItemCard from "../../components/itemCard";
+import CarouselComponent from "../../components/carousel";
 
 const Home = () => {
+  /**
+   * Netlify CMS's accept invite link land on home page.
+   * This redirection takes it to the right place(/admin).
+   */
 
-	/**
-	 * Netlify CMS's accept invite link land on home page.
-	 * This redirection takes it to the right place(/admin).
-	 */
+  useEffect(() => {
+    if (
+      window !== undefined &&
+      window.location.href.includes("#invite_token")
+    ) {
+      const { href } = window.location;
+      window.location.href = `${href.substring(
+        0,
+        href.indexOf("#")
+      )}admin${href.substring(href.indexOf("#"))}`;
+    }
+  }, []);
 
-	useEffect(() => {
-		if (window !== undefined && window.location.href.includes('#invite_token')) {
-			const { href } = window.location;
-			window.location.href= `${href.substring(0, href.indexOf('#'))}admin${href.substring(href.indexOf('#'))}`;
-		}
-	},[]);
+	const carouselImages = [
+		'https://picsum.photos/2000/800',
+		'https://picsum.photos/2000/800',
+		'https://picsum.photos/2000/800',
+	]
 
-	return (
-		<div>
-			<div id="logo-carousel" class="carousel slide">
-				<div class="carousel-inner">
-					<div class="carousel-item active">
-						<img style={{maxHeight: "70vh", objectFit: "cover"}} src="https://picsum.photos/2000" class="d-block w-100" />
-					</div>
-					<div class="carousel-item">
-						<img style={{maxHeight: "70vh", objectFit: "cover"}} src="https://picsum.photos/2000" class="d-block w-100" />
-					</div>
-					<div class="carousel-item">
-						<img style={{maxHeight: "70vh", objectFit: "cover"}} src="https://picsum.photos/2000" class="d-block w-100" />
-					</div>
-				</div>
-				<button class="carousel-control-prev" type="button" data-bs-target="#logo-carousel" data-bs-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Previous</span>
-				</button>
-				<button class="carousel-control-next" type="button" data-bs-target="#logo-carousel" data-bs-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Next</span>
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+			<CarouselComponent images={carouselImages} />
+      <ItemCard title={"Test"} description={"This is a test"} />
+    </div>
+  );
 };
 
 export default Home;
