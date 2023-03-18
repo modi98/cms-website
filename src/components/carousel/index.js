91fce817dcfbfe36ea0aa0414ff8b560cfe8b58b
Mauricio Alvarado <mauricio.alvaradoh98@gmail.com>
2023-03-17 22:48:16 -0600

    fix(carousel): item height

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,11 +1,12 @@
-import { Carousel } from "react-bootstrap";
+import style from './style';
+import { Carousel } from 'react-bootstrap';
 
 const CarouselComponent = ({ images }) => {
   return (
     <Carousel>
       {images.map((item, i) => (
-        <Carousel.Item key={i}>
-          <img className="d-block w-100 " src={item} alt="First slide" />
+        <Carousel.Item key={i} style={{ height: '90vh', maxHeight: '600px'}}>
+          <img style={{ height: 'inherit', objectFit: 'cover' }} className="d-block w-100 " src={item} alt="First slide" />
           <Carousel.Caption>
             <h3>Slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
