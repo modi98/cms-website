4738f8eda156d1db3a5f3085358369284d5e8c90
Mauricio Alvarado <mauricio.alvaradoh98@gmail.com>
2023-04-04 19:28:23 -0600

    feat: contact form responsive

--- src/components/contactForm/index.js
+++ src/components/contactForm/index.js
@@ -1,31 +1,34 @@
+import { Container, Row, Col } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import ItemButton from "../../components/button";
 
 const contactForm = () => {
   return (
-    <div className='d-flex justify-content-evenly flex-wrap mx-4'>
-      <div className='pe-1 m-2' style={{ width: "45%"}}>
-        <h2>Información de contacto</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ipsum est. 
-          Nulla ac massa a erat rutrum tincidunt. Nam ultrices eleifend suscipit. 
-          Etiam consequat, dui eu bibendum hendrerit, mi purus lobortis diam, sit amet 
-          congue arcu turpis ac ipsum. Suspendisse non commodo leo, vel congue turpis.</p>
-      </div>
-      <div className='mt-5 m-2' style={{ width: "45%"}}>
-        <Form>
-          <Form.Group className="mb-3" controlId="nameForm">
-            <Form.Control type="text" placeholder="Nombre" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="emailForm">
-            <Form.Control type="email" placeholder="Correo" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="messageForm">
-            <Form.Control as="textarea" placeholder="Mensaje" rows={3} />
-          </Form.Group>
-          <ItemButton title={"Enviar"} />
-        </Form>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col md={6}>
+          <h2>Información de contacto</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ipsum est.
+            Nulla ac massa a erat rutrum tincidunt. Nam ultrices eleifend suscipit.
+            Etiam consequat, dui eu bibendum hendrerit, mi purus lobortis diam, sit amet
+            congue arcu turpis ac ipsum. Suspendisse non commodo leo, vel congue turpis.</p>
+        </Col>
+        <Col md={6}>
+          <Form>
+            <Form.Group className="mb-3" controlId="nameForm">
+              <Form.Control type="text" placeholder="Nombre" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="emailForm">
+              <Form.Control type="email" placeholder="Correo" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="messageForm">
+              <Form.Control as="textarea" placeholder="Mensaje" rows={3} />
+            </Form.Group>
+            <ItemButton title={"Enviar"} />
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
