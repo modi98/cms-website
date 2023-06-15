5fa3dbf46f24cfdc70b9010560098a4c1860c14b
Mauricio Alvarado <mauricio.alvaradoh98@gmail.com>
2023-06-14 20:19:01 -0600

    fix: fallback for load

--- src/routes/products/index.js
+++ src/routes/products/index.js
@@ -2,18 +2,29 @@ import { usePrerenderData } from "@preact/prerender-data-provider";
 import { h } from "preact";
 import Spinner from "react-bootstrap/Spinner";
 import SearchProducts from "../../components/searchProducts";
+import { useState } from "preact/hooks";
 
 const Products = (props) => {
   const [data, isLoading] = usePrerenderData(props);
+  const [loadFallback, setLoadFallback] = useState(false);
+
+  setTimeout(() => setLoadFallback(true), 5000);
+
   if (!isLoading && data) {
     return <SearchProducts data={data} />;
   }
   return (
-    <div className="spinner-container">
-      <Spinner animation="border" role="status" className="m-auto">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-    </div>
+    <>
+      {!loadFallback ? (
+        <div className="spinner-container">
+          <Spinner animation="border" role="status" className="m-auto">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      ) : (
+        <div className="text-center">No hay productos</div>
+      )}
+    </>
   );
 };
 
